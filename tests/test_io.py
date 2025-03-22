73cfd7b3f79075ffb7a940b768fdaa49a48d2084
jperradin <julien.perradin@protonmail.com>
2025-03-22 17:03:52 +0100

    removed unecessary files

--- tests/test_io.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from reve.io.reader import ReaderFactory
-from reve.io.reader.xyz_reader import XYZReader
-from reve.io.reader.lammps_reader import LAMMPSReader
-
-# TODO: implement pytest tests
