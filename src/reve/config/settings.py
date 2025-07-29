a3e894ef6a3a33295d5757991f69f5ce808da69d
Julien Perradin <julien.perradin@protonmail.com>
2025-07-29 16:58:59 +0200

    updated settings

--- src/reve/config/settings.py
+++ src/reve/config/settings.py
@@ -515,7 +515,9 @@ class SettingsBuilder:
             # Create a SQFFTAnalysisSettings object if not create
             sqfft_settings = SQFFTAnalysisSettings()
             analysis.sqfft_settings = sqfft_settings
-        if (analysis.with_structural_units or analysis.with_all) and analysis.strunits_settings is None:
+        if (
+            analysis.with_structural_units or analysis.with_all
+        ) and analysis.strunits_settings is None:
             # Create a SQFFTAnalysisSettings object if not create
             strunits_settings = STRUNITSAnalysisSettings(
                 units_to_calculate=[
@@ -530,17 +532,21 @@ class SettingsBuilder:
                 ]
             )
             analysis.strunits_settings = strunits_settings
-        if (analysis.with_connectivity or analysis.with_all) and analysis.connect_settings is None:
+        if (
+            analysis.with_connectivity or analysis.with_all
+        ) and analysis.connect_settings is None:
             # Create a SQFFTAnalysisSettings object if not create
             connect_settings = CONNAnalysisSettings(
                 networking_species="Si",
                 bridging_species="O",
             )
             analysis.connect_settings = connect_settings
-        if( analysis.with_polyhedricity or analysis.with_all) and analysis.poly_settings is None:
+        if (
+            analysis.with_polyhedricity or analysis.with_all
+        ) and analysis.poly_settings is None:
             # Create a POLYAnalysisSettings object if not create
             poly_settings = POLYAnalysisSettings(
-                central_species="Si", vertices_species="O", max_c=0.2, print_forms=True
+                central_species="Si", vertices_species="O", max_c=0.2, print_forms=False
             )
             analysis.poly_settings = poly_settings
 
